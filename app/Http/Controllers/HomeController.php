41be5112947fdafab9c7777cbfc470eb5a465b38
louis <keat_09@hotmail.com>
2020-05-02 02:20:12 +0800

    session

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,21 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        
+        // session(['peter' => 'awesome']); //global function to set session
+        $request->session()->put(['louis' => 'student']); // set session
+
+        $result = $request->session()->get('louis'); //get session
+        // $result = $request->session()->all()); //get all session
+
+        // $request->session()->forget('louis'); //remove session
+        // $request->session()->flush(); //remove all session
+        
+        // $request->session()->flash(['message', 'Post created successful!']); //flash session by request, second request will removed
+
+        return $result;
+        // return view('home');
     }
 }
