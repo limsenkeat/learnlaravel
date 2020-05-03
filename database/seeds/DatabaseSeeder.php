3e2bfd94b8d7288a4dc91353db7f56e1cac5549e
Lim Sen Keat <keat_09@hotmail.com>
2020-05-03 22:38:17 +0800

    seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $this->call(UserTableSeeder::class);
+
+        factory(App\User::class, 3)->create()->each(function($user){
+
+            $user->post()->save(factory(App\Post::class)->make());
+
+        }); //create 3 user with 1 post
     }
 }
